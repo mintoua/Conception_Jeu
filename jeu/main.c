04fe2962622b8c1d639c71421aa877b00f9900f3
Parsath <47742269+Parsath@users.noreply.github.com>
2019-03-02 01:05:18 +0100

    J'ai amélioré le menu
    Il ne manque plus que quelques implémentations pour qu'il soit complet, j'ai déjà préparé les Fichiers nécessaire pour rajouter la " Difficulté" et le menu " Nouveau jeu" ou " Continuer "

--- jeu/main.c
+++ jeu/main.c
@@ -1,5 +1,4 @@
-#include "entete.h"
-
+#include"entete.h"
 
 int main(void){
 
@@ -12,7 +11,8 @@ int main(void){
 	SDL_WM_SetCaption("TunCity",NULL);
 	SDL_WM_SetIcon(IMG_Load("gamepad.png"),NULL);
 
-	SDL_Surface *screen=NULL,*background=NULL,*play=NULL,*settings=NULL,*quit=NULL,*Sbackground=NULL,*Svolume=NULL,*Scharacters=NULL,*Sback=NULL;
+	SDL_Surface *screen=NULL,*background=NULL,*play=NULL,*settings=NULL,*quit=NULL,*Sbackground=NULL,*Svolume=NULL,*Scharacters=NULL,*Sback=NULL,*Sdifficulty=NULL,*v1=NULL,*v2=NULL,*v3=NULL,*v4=NULL,*v5=NULL,*v6=NULL,*v7=NULL,*v8=NULL,*v9=NULL,*v10=NULL,*v11=NULL;
+
 	screen=SDL_SetVideoMode(1365,768,32,SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_FULLSCREEN);
 
 	if(!screen){
@@ -30,11 +30,23 @@ int main(void){
 	Sback=IMG_Load("setBack.png");
 	Scharacters=IMG_Load("setCharacters.png");
 	background=IMG_Load("Tuncity_menu_3.png");
+	Sdifficulty=IMG_Load("setDifficulty.png");
 	play=IMG_Load("Play.png");
 	settings=IMG_Load("Settings.png");
 	quit=IMG_Load("Quit.png");
+	v1=IMG_Load("v1.png");
+	v2=IMG_Load("v2.png");
+	v3=IMG_Load("v3.png");
+	v4=IMG_Load("v4.png");
+	v5=IMG_Load("v5.png");
+	v6=IMG_Load("v6.png");
+	v7=IMG_Load("v7.png");
+	v8=IMG_Load("v8.png");
+	v9=IMG_Load("v9.png");
+	v10=IMG_Load("v10.png");
+	v11=IMG_Load("v11.png");
 
-	if( (background==NULL) || (Sbackground==NULL) || (Svolume==NULL) || (Sback==NULL) || (Scharacters==NULL) || (play==NULL) || (settings==NULL) || (quit==NULL) ){
+	if( (background==NULL) || (Sbackground==NULL) || (Svolume==NULL) || (Sback==NULL) || (Scharacters==NULL) || (play==NULL) || (settings==NULL) || (quit==NULL) || (v1==NULL) || (v2==NULL) || (v3==NULL) || (v4==NULL) || (v5==NULL) || (v6==NULL) || (v7==NULL) || (v8==NULL) || (v9==NULL) || (v10==NULL) || (v11==NULL) || (Sdifficulty==NULL)){
 		fprintf(stderr,"Unable to load png: %s\n",SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
@@ -57,12 +69,12 @@ int main(void){
 
 	SDL_Flip(screen);
 
-	SDL_Rect pbackground,psettings,pplay,pquit,pvolume,pback,pcharacters;
+	SDL_Rect pdifficulty,pbackground,psettings,pplay,pquit,pvolume,pback,pcharacters;
 	
 	pquit.x=pplay.x=psettings.x=145.83;
 	psettings.w=pplay.w=pquit.w=478.49;
 	psettings.h=pplay.h=pquit.h=114.76;
-	pcharacters.h=pvolume.h=pback.h=62;	
+	pcharacters.h=pvolume.h=pback.h=pdifficulty.h=62;	
 
 	pcharacters.x=465.5;
 	pcharacters.y=414;
@@ -72,9 +84,13 @@ int main(void){
 	pvolume.y=276;
 	pvolume.w=283.5;
 
-	pback.x=613;
-	pback.y=542;
-	pback.w=180;
+	pdifficulty.x=464;
+	pdifficulty.y=543;
+	pdifficulty.w=pcharacters.w;
+
+	pback.x=612.7;
+	pback.y=660.7;
+	pback.w=179.4;
 	
 	psettings.y=391.52;
 	pplay.y=261.66;
@@ -83,13 +99,14 @@ int main(void){
 	pbackground.x=0;
 	pbackground.y=0;
 
-
 	SDL_BlitSurface(background,NULL,screen,&pbackground);
 	SDL_Flip(screen);
 
+	int volume=128;
 	int continuer=1;
 	int etat=1;
 	int S_etat=0;
+	int Vetat=11;
 	SDL_Event event;
 	
 	while(continuer){
@@ -103,6 +120,38 @@ int main(void){
 					case SDLK_ESCAPE:
 						continuer=0;
 						break;
+					case SDLK_RIGHT:
+						if( (S_etat==2) && Vetat<11 ){
+							Mix_PlayChannel(-1, bouton_sclic, 0);
+						 	Vetat++;
+							Mix_VolumeMusic(volume+=11);
+							Mix_VolumeChunk(bouton_sclic,volume);
+							Mix_VolumeChunk(bouton_clic,volume);
+						}
+					break;
+					case SDLK_LEFT:
+						if( (S_etat==2) && Vetat>1 ){
+							Mix_PlayChannel(-1, bouton_sclic, 0);
+						 	Vetat--;
+							Mix_VolumeMusic(volume-=11);
+							Mix_VolumeChunk(bouton_sclic,volume);
+							Mix_VolumeChunk(bouton_clic,volume);
+							if(Vetat==1){
+								Mix_VolumeMusic(-1);
+								Mix_VolumeChunk(bouton_clic,0);
+								Mix_VolumeChunk(bouton_sclic,0);
+							}
+						}
+					break;
+					case SDLK_BACKSPACE:
+						if(S_etat==2){
+							S_etat=1;
+							etat=8;
+						}
+						else if(S_etat==1){
+							S_etat=0;
+							etat=1;
+						}
 					case SDLK_DOWN:
 						if( (S_etat==0) && (etat==1) ){
 							if(etat!=2){
@@ -114,32 +163,32 @@ int main(void){
 						}
 
 						else if( (S_etat==0) && (etat==2) ){
-							if(etat!=4){
+							 	if(etat!=3){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
 							event.motion.x=psettings.x;
 							event.motion.y=psettings.y;
-							etat=4;
-						}
-
-						else if( (S_etat==0) && (etat==4) ){
-							if(etat!=3){
-								Mix_PlayChannel(-1, bouton_sclic, 0);
-							}
-							event.motion.x=pquit.x;
-							event.motion.y=pquit.y;
 							etat=3;
 						}
 
 						else if( (S_etat==0) && (etat==3) ){
+							if(etat!=4){
+								Mix_PlayChannel(-1, bouton_sclic, 0);
+							}
+							event.motion.x=pquit.x;
+							event.motion.y=pquit.y;
+							etat=4;
+						}
+
+						else if( (S_etat==0) && (etat==4) ){
 							if(etat!=2){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
-							event.motion.x=pplay.x;s
+							event.motion.x=pplay.x;
 							event.motion.y=pplay.y;
 							etat=2;
 						}
-						
+
 						else if( (S_etat==1) && (etat==8) ){
 							if(etat!=5){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
@@ -159,6 +208,15 @@ int main(void){
 						}
 
 						else if( (S_etat==1) && (etat==6) ){
+							if(etat!=9){
+								Mix_PlayChannel(-1, bouton_sclic, 0);
+							}
+							event.motion.x=pdifficulty.x;
+							event.motion.y=pdifficulty.y;
+							etat=9;
+						}
+
+						else if( (S_etat==1) && (etat==9) ){
 							if(etat!=7){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
@@ -178,24 +236,24 @@ int main(void){
 					break;
 					case SDLK_UP:
 						if( (S_etat==0) && (etat==1) ){
-							if(etat!=3){
+							if(etat!=4){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
 							event.motion.x=pquit.x;
 							event.motion.y=pquit.y;
-							etat=3;
+							etat=4;
 						}
 
 						else if( (S_etat==0) && (etat==2) ){
-							if(etat!=3){
+							if(etat!=4){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
 							event.motion.x=pquit.x;
 							event.motion.y=pquit.y;
-							etat=3;
+							etat=4;
 						}
 
-						else if( (S_etat==0) && (etat==4) ){
+						else if( (S_etat==0) && (etat==3) ){
 							if(etat!=2){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
@@ -204,13 +262,13 @@ int main(void){
 							etat=2;
 						}
 
-						else if( (S_etat==0) && (etat==3) ){
-							if(etat!=4){
+						else if( (S_etat==0) && (etat==4) ){
+							if(etat!=3){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
 							event.motion.x=psettings.x;
 							event.motion.y=psettings.y;
-							etat=4;
+							etat=3;
 						}
 						
 						else if( (S_etat==1) && (etat==8) ){
@@ -240,7 +298,7 @@ int main(void){
 							event.motion.y=pvolume.y;
 						}
 
-						else if( (S_etat==1) && (etat==7) ){
+						else if( (S_etat==1) && (etat==9) ){
 							if(etat!=6){
 								Mix_PlayChannel(-1, bouton_sclic, 0);
 							}
@@ -248,9 +306,18 @@ int main(void){
 							event.motion.y=pcharacters.y;
 							etat=6;
 						}
+						else if( (S_etat==1) && (etat==7) ){
+							if(etat!=9){
+								Mix_PlayChannel(-1, bouton_sclic, 0);
+							}
+							event.motion.x=pdifficulty.x;
+							event.motion.y=pdifficulty.y;
+							etat=9;
+						}
+
 					break;
 					case SDLK_RETURN:
-						if( (etat==4) && (S_etat==0)){
+						if( (etat==3) && (S_etat==0)){
 							Mix_PlayChannel(-1, bouton_clic, 0);
 							S_etat=1;
 							etat=8;	
@@ -260,10 +327,18 @@ int main(void){
 							S_etat=0;
 							etat=1;	
 						}
-						else if( (etat==3) && (S_etat==0)){
+						else if( (etat==4) && (S_etat==0)){
 							Mix_PlayChannel(-1, bouton_clic, 0);
 							continuer=0;
 						}
+						else if( (etat==5) && (S_etat==1) ){
+							Mix_PlayChannel(-1, bouton_clic, 0);
+							S_etat=2;
+						}
+						else if( (etat==9) && (S_etat==1) ){
+							Mix_PlayChannel(-1, bouton_clic, 0);
+							S_etat=3;
+						}						
 
 					break;
 				}
@@ -280,17 +355,23 @@ int main(void){
 				}
 
 				else if( (event.motion.x>=pquit.x) && (event.motion.x<=(pquit.x+478.49) ) && (event.motion.y>=pquit.y) && (event.motion.y<=(pquit.y+114.76) ) && (S_etat==0) ){
+					if(etat!=4){
+						Mix_PlayChannel(-1, bouton_sclic, 0);
+					}
+					etat=4;
+				}
+
+				else if( (event.motion.x>=psettings.x) && (event.motion.x<=(psettings.x+478.49) ) && (event.motion.y>=psettings.y) && (event.motion.y<=(psettings.y+114.76) ) && (S_etat==0) ){
 					if(etat!=3){
 						Mix_PlayChannel(-1, bouton_sclic, 0);
 					}
 					etat=3;
 				}
-
-				else if( (event.motion.x>=psettings.x) && (event.motion.x<=(psettings.x+478.49) ) && (event.motion.y>=psettings.y) && (event.motion.y<=(psettings.y+114.76) ) && (S_etat==0) ){
-					if(etat!=4){
+				else if( (event.motion.x>=pdifficulty.x) && (event.motion.x<=(pdifficulty.x+pdifficulty.w) ) && (event.motion.y>=pdifficulty.y) && (event.motion.y<=(pdifficulty.y+pdifficulty.h) ) && (S_etat==1) ){
+					if(etat!=9){
 						Mix_PlayChannel(-1, bouton_sclic, 0);
 					}
-					etat=4;
+					etat=9;	
 				}
 
 				else if(!( (event.motion.x>=pquit.x) && (event.motion.x<=(pquit.x+478.49) ) && (event.motion.y>=pquit.y) && (event.motion.y<=(pquit.y+114.76) ) ) && !( (event.motion.x>=pquit.x) && (event.motion.x<=(pquit.x+478.49) ) && (event.motion.y>=pquit.y) && (event.motion.y<=(pquit.y+114.76) ) ) && !( (event.motion.x>=pplay.x) && (event.motion.x<=(pplay.x+478.49) ) && (event.motion.y>=pplay.y) && (event.motion.y<=(pplay.y+114.76) ) ) && (S_etat==0) )
@@ -315,9 +396,9 @@ int main(void){
 						Mix_PlayChannel(-1, bouton_sclic, 0);
 					}
 					etat=7;
-				}		
+				}	
 
-				else if( !( (event.motion.x>=pvolume.x) && (event.motion.x<=(pvolume.x+283.5) ) && (event.motion.y>=pvolume.y) && (event.motion.y<=(pvolume.y+62) ) ) && !( (event.motion.x>=pback.x) && (event.motion.x<=(pback.x+pback.w) ) && (event.motion.y>=pback.y) && (event.motion.y<=(pback.y+pcharacters.h) ) ) && !( (event.motion.x>=pcharacters.x) && (event.motion.x<=(pcharacters.x+pcharacters.w) ) && (event.motion.y>=pcharacters.y) && (event.motion.y<=(pcharacters.y+pcharacters.h) ) ) && (S_etat==1) ){
+				else if( !( (event.motion.x>=pvolume.x) && (event.motion.x<=(pvolume.x+283.5) ) && (event.motion.y>=pvolume.y) && (event.motion.y<=(pvolume.y+62) ) ) && !( (event.motion.x>=pdifficulty.x) && (event.motion.x<=(pdifficulty.x+pdifficulty.w) ) && (event.motion.y>=pdifficulty.y) && (event.motion.y<=(pdifficulty.y+pdifficulty.h) )) && !( (event.motion.x>=pback.x) && (event.motion.x<=(pback.x+pback.w) ) && (event.motion.y>=pback.y) && (event.motion.y<=(pback.y+pcharacters.h) ) ) && !( (event.motion.x>=pcharacters.x) && (event.motion.x<=(pcharacters.x+pcharacters.w) ) && (event.motion.y>=pcharacters.y) && (event.motion.y<=(pcharacters.y+pcharacters.h) ) ) && (S_etat==1) ){
 					etat=8;
 				}		
 				break;
@@ -338,11 +419,20 @@ int main(void){
 					etat=1;			
 					}
 				}
+				else if( (event.button.x>=pdifficulty.x) && (event.button.x<=(pdifficulty.x+pdifficulty.w) ) && (event.button.y>=pdifficulty.y) && ( event.button.y<=(pdifficulty.y+pdifficulty.h) ) && (S_etat==1) && (event.button.button=SDL_BUTTON_LEFT)){
+					Mix_PlayChannel(-1, bouton_sclic, 0);
+					S_etat=3;
+				}
 				else if( (event.button.x>=pquit.x) && (event.button.x<=(pquit.x+pquit.w) ) && (event.button.y>=pquit.y) && (event.button.y<=(pquit.y+pquit.h) ) && (event.button.button=SDL_BUTTON_LEFT) && (S_etat==0)){
 					Mix_PlayChannel(-1, bouton_clic, 0);{
 					continuer=0;		
 					}
 				}
+				else if( (event.button.x>=pvolume.x) && (event.button.x<=(pvolume.x+283.5) ) && (event.button.y>=pvolume.y) && (event.button.y<=(pvolume.y+62) ) && (S_etat==1) && (event.button.button=SDL_BUTTON_LEFT)){
+					Mix_PlayChannel(-1, bouton_sclic, 0);
+					S_etat=2;
+				}
+				
 
 				break;
 		}
@@ -359,12 +449,12 @@ int main(void){
 			SDL_BlitSurface(play,NULL,screen,&pbackground);
 			SDL_Flip(screen);
 		}
-		else if(etat==3 && S_etat==0){
+		else if(etat==4 && S_etat==0){
 			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
 			SDL_BlitSurface(quit,NULL,screen,&pbackground);
 			SDL_Flip(screen);
 		}
-		else if(etat==4 && S_etat==0){
+		else if(etat==3 && S_etat==0){
 			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
 			SDL_BlitSurface(settings,NULL,screen,&pbackground);
 			SDL_Flip(screen);
@@ -390,9 +480,82 @@ int main(void){
 			SDL_BlitSurface(Sbackground,NULL,screen,&pbackground);
 			SDL_Flip(screen);	
 		}
+		else if(etat==9 && S_etat==1){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(Sdifficulty,NULL,screen,&pbackground);
+			SDL_Flip(screen);
+		}
+		if(Vetat==1 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,0,255,255));
+			SDL_BlitSurface(v1,NULL,screen,&pbackground);
+			SDL_Flip(screen);
+			SDL_Flip(screen);
+		}
+		else if(Vetat==2 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v2,NULL,screen,&pbackground);
+			SDL_Flip(screen);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==3 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v3,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==4 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v4,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==5 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v5,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==6 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v6,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==7 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v7,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==8 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v8,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==9 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v9,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==10 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v10,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
+		else if(Vetat==11 && S_etat==2){
+			SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+			SDL_BlitSurface(v11,NULL,screen,&pbackground);
+			SDL_Flip(screen);	
+		}
 	}
 
 
+	SDL_FreeSurface(v1);
+	SDL_FreeSurface(v2);
+	SDL_FreeSurface(v3);
+	SDL_FreeSurface(v4);
+	SDL_FreeSurface(v5);
+	SDL_FreeSurface(v6);
+	SDL_FreeSurface(v7);
+	SDL_FreeSurface(v8);
+	SDL_FreeSurface(v9);
+	SDL_FreeSurface(v10);
+	SDL_FreeSurface(v11);
 	SDL_FreeSurface(play);
 	SDL_FreeSurface(settings);
 	SDL_FreeSurface(quit);
