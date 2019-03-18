1fe672e359182da528f7a64c77f0ed5ce9c2044e
halimkacem <eya.souissi@esprit.tn>
2019-03-18 15:14:36 -0700

    voila la mise a jour

--- taches_headers/background/background.h
+++ taches_headers/background/background.h
@@ -1,5 +1,12 @@
-#ifndef BACKGROUND_H_
-#define BACKGROUND_H_
+#ifndef BK_H_
+#define BK_H_
+#include <stdio.h>
+ #include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+ #include <SDL/SDL_mixer.h>
+#include "persop.h"
 
 typedef struct
 {
@@ -16,6 +23,20 @@ typedef struct
 	Mix_Music * son_but;
 }button;
 
+
+typedef struct BACKGROUND
+{
+SDL_Surface*  bg;
+SDL_Rect position;
+SDL_Rect scroll;
+int stage;
+int position_actuel;
+}BACKGROUND;
+
+void init_bk(BACKGROUND* b);
+void afficher_bk(BACKGROUND bk,SDL_Surface *fenetre);
+
+void scrollingbg(int sens,BACKGROUND *bg);
 
 
 
